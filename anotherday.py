cb1aa543024ac8d96d07de5903eb8e4f3b29431a
tenzing155 <tenzing155@gmail.com>
2024-07-11 15:58:59 +0545

    new

---
+++ anotherday.py
@@ -0,0 +1,71 @@
+# data = [
+#     {'username': 'ram', 'password': 'ram123'},
+#     {'username': 'sophia', 'password': 'sophia123'},
+# ]
+# username = input("Enter your username: ")
+# password = input("Enter your password: ")
+# if data[0]['username']== username and data[0]['password'] == password:
+#     print("You are logged in.")
+# elif data[1]['username'] == username and data[1]['username'] == password:
+#     print('You are logged in')
+# else:
+#     print("Wrong email and password")
+
+
+# x = 10
+# y = 30
+# z = 40
+# #40,30,10
+# if x>y and x>z:
+#     if y>z:
+#         print(x,y,z)
+#     else:
+#         print(x,z,y)
+# elif y>x and y>z:
+#     if x>z:
+#         print(y,x,z)
+#     else:
+#         print(y,z,x)
+# else:
+#     if y>x:
+#         print(z,y,x)
+#     else:
+#         print(z,y,x)
+
+
+# x = 10
+# y = 30
+# z = 40
+# #10,30,40
+# if x<y and x<z:
+#     if y<z:
+#         print(x,y,z)
+#     else:
+#         print(x,z,y)
+# elif y<x and y<z:
+#     if x<z:
+#         print(y,x,z)
+#     else:
+#         print(y,z,x)
+# else:
+#     if y<x:
+#         print(z,y,x)
+#     else:
+#         print(z,y,x)
+
+
+# age = int(input("Enter your age: "))
+# if age >= 18 and age < 40:
+#     print("Welcome to party!")
+#     if age >=18 and age <25:
+#         print("You can only drink beer")
+#     elif age >=25 and age <30:
+#         print("You can only drink whiskey")
+#     elif age >=30 and age <40:
+#         print("You can only drink wine")
+# else:
+#     if age > 40:
+#         print("You are old")
+#     else:
+#         print("You are too young")
+
--- python demo/today.py
+++ python demo/today.py
@@ -17,24 +17,37 @@
 # else:
 #     print("You are a child")
 
-print("ATM")
-print("Enter your pin.")
-pin = int(input())
-if pin == 1234:
-    print("Welcome user.")
-    print("withdraw or  balance inquiry")
-    w = input()
-    total = 10000
-    if w == "withdraw":
-        print(f"Your total amount is {total}")
-        print(f"Enter the amount you want to withdraw.")
-        amount = int(input())
-        bal = total - amount
-        print(f"Your balance is {bal}")
-    elif w == "balance inquiry":
-        print(f"Your total is {total}")
-else :
-    print("Pin did not match")
-    
-    
+# print("ATM")
+# print("Enter your pin.")
+# pin = int(input())
+# if pin == 1234:
+#     print("Welcome user.")
+#     print("withdraw or  balance inquiry")
+#     w = input()
+#     total = 10000
+#     if w == "withdraw":
+#         print(f"Your total amount is {total}")
+#         print(f"Enter the amount you want to withdraw.")
+#         amount = int(input())
+#         bal = total - amount
+#         print(f"Your balance is {bal}")
+#     elif w == "balance inquiry":
+#         print(f"Your total is {total}")
+# else :
+#     print("Pin did not match")
 
+# bank = "Bank of kathmandu"
+# name = input("Enter your name") 
+# address = input("Enter your address")   
+# document = input("Enter your document")    
+
+# if document == "citizenship" or document == "passport":
+#     print("You are eligible to open account")
+# else:
+#     print("Invalid document")
+
+# num = int(input("Enter any number : "))
+# if num % 3 == 0 and num % 5 == 0:
+#     print("It is divisble by both 3 and 5")
+# else:
+#     print("It is not divisible")
