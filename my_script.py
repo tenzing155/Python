21027c65d48cb7f6bccf2290748e44fd3a40d58e
tenzing155 <tenzing155@gmail.com>
2024-08-20 18:05:44 +0545

    new

---
+++ my_script.py
@@ -0,0 +1,29 @@
+#JavaScript Object Notation - data
+# convert from json to dictinoary
+# import json
+
+# some JSON:
+# x = '{ "name":"John", "age":30, "city":"New York"}'
+
+# parse x:
+# y = json.loads(x)
+
+# the result is a Python dictionary:
+# print(y['name'])
+
+#convert from dictionary to json
+# import json
+
+# a Python object (dict):
+# import json
+# x = {
+#   "name": "John",
+#   "age": 30,
+#   "city": "New York"
+# }
+
+# convert into JSON:
+# y = json.dumps(x)
+
+# the result is a JSON string:
+# print(y)
